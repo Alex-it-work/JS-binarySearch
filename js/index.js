c8b9a43e2f7e0d5740c1ba6c0579e29c85fa88ad
Alex-it-work <Mr.Kirieiev@gmail.com>
2021-06-15 00:02:16 +0300

    Fatalitygit add .!

--- js/index.js
+++ js/index.js
@@ -2,46 +2,38 @@
 // Использовать бинарный поиск, чтобы отгадать загаданное пользователем число от 0 до 100.
 // Задавать вопрос с помощью confirm.
 
-const userInputValue = askUserToInputValue();
+binarySearch();
 
-try {
-  valueValidator(userInputValue);
-  binarySearch(userInputValue);
-} catch (error) {
-  if (error instanceof TypeError) {
-    alert("Input a number value pls");
-  }
-  if (error instanceof RangeError) {
-    alert("Input finite, integer value 0...100 pls");
-  }
-}
-
-function askUserToInputValue() {
-  return Number(prompt("Hello! \n Input any value 0...100 pls:"));
-}
-
-function valueValidator(value) {
-  if (isNaN(value)) {
-    throw new TypeError("The input value must be a number ");
-  }
-  if (value < 0 || value > 100) {
-    throw new RangeError("The input value must be from 0 to 100");
-  }
-  return value;
-}
-
-function binarySearch(value) {
+function binarySearch() {
   let fromValue = 0;
   let toValue = 100;
   let middle = 50;
-  while (middle !== value) {
+
+  while (middle > fromValue + 1) {
     if (confirm(`Is it less than ${middle}?`)) {
       toValue = middle - 1;
       middle = Math.floor((fromValue + toValue) / 2);
     } else {
-      fromValue = middle + 1;
+      fromValue = middle;
       middle = Math.floor((fromValue + toValue) / 2);
     }
   }
-  alert(`Your value is ${middle}`);
+
+  if (confirm(`Is it more than ${middle}?`)) {
+    alert(`Your value is ${toValue}`);
+    return;
+  }
+
+  if (middle === fromValue) {
+    alert(`Your value is ${middle}`);
+    return;
+  }
+
+  if (confirm(`Is it more than ${fromValue}?`)) {
+    alert(`Your value is ${middle}`);
+    return;
+  }
+
+  alert(`Your value is ${fromValue}`);
 }
+//Виктория, 3 рогатки пришлось ставить в конце кода, зато ОНО бинарное получилось до конца
